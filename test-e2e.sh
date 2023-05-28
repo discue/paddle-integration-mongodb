75424ebef906731daea933a5bac66b113185a819
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-05-28 17:44:54 +0200

    chore: remove firebase artifacts

--- test-e2e.sh
+++ test-e2e.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export FIREBASE_AUTH_EMULATOR_HOST=localhost:9099
-export FIRESTORE_EMULATOR_HOST=localhost:11111
-export FIREBASE_CONFIG='{ "projectId": "discue-io-dev", "storageBucket": "discue-io-dev.appspot.com" }'
 export NODE_ENV='e2e'
 
 function cancel {
