8ddce30512580ce33dffb8b0688537c0651bbc81
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-09-02 11:07:29 +0200

    tests: reduce single test timeout

--- playwright.config.js
+++ playwright.config.js
@@ -9,7 +9,7 @@ const config = {
     testMatch: '*.spec.js',
     reportSlowTests: null,
     workers: 1,
-    timeout: 1000 * 60 * 5,
+    timeout: 1000 * 60 * 1,
     fullyParallel: false,
     forbidOnly: !!process.env.CI,
     globalSetup: require.resolve('./test-e2e/before-all.js'),
