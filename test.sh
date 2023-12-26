a73b7055f23e716091a7bcadcd1131291a5b1e70
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-12-26 21:18:19 +0100

    chore: make sure telemetry can be disabled with noop module

--- test.sh
+++ test.sh
@@ -3,3 +3,24 @@
 export NODE_ENV='ci'
 
 npm run test
+
+# check whether we are connected to the internet 
+# before downloading dependencies
+resetDependencies() {
+    # reset tracing dependencies only if we are not running on GH
+    if [[ -z "${GITHUB_ACTIONS}" ]]; then
+        npm un @discue/open-telemetry-tracing
+        npm -D i @discue/open-telemetry-tracing
+    fi
+}
+
+curl --head www.google.com &>"/dev/null"
+if [[ "${?}" == 0 ]]; then
+    trap resetDependencies ERR EXIT
+
+    # test with noop tracing library now
+    npm i -D @discue/open-telemetry-tracing@npm:@discue/open-telemetry-tracing-noop
+    npm run test
+
+    resetDependencies
+fi
