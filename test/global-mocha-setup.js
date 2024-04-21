30d5c313e22b7e121af61d446c1a24b5083d3e09
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-04-21 22:37:46 +0200

    tests: set mongodb parameters

--- test/global-mocha-setup.js
+++ test/global-mocha-setup.js
@@ -1,12 +1,19 @@
-const { MongoMemoryServer } = require('mongodb-memory-server')
+const { MongoMemoryReplSet } = require('mongodb-memory-server')
 
 let mongod
 
 before(async function () {
-    // This will create an new instance of "MongoMemoryServer" and automatically start it
-    mongod = await MongoMemoryServer.create({
-        instance: {
-            port: 27017
+    // This will create an new instance of 'MongoMemoryServer' and automatically start it
+    mongod = await MongoMemoryReplSet.create({
+        instanceOpts: [{
+            args: ['--setParameter', 'notablescan=1'],
+            port: 27021
+        }],
+        replSet: {
+            count: 1
+        },
+        binary: {
+            version: '6.0.0'
         }
     })
 })
